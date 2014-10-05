2426e92b0ed0fd4c7bd48a255cc26a78905c27dc
Gemma Barlow <gemma@harrys.com>
2014-10-04 20:51:57 -0400

    Setup basic UITableView
    - Added UITableViewDelegate & Datasource methods and hooked them up to ViewController (via the Storyboard)
    - Added ExerciseTableViewCell and associated it with ViewController in the Storyboard
    - Embedded UITableView in Navigation Controller, (creating the NavBar @ the top of the screen) - (Editor > Embed In > NavigationController)
    
    Signed-off-by: Gemma Barlow <gemma@harrys.com>

---
+++ Office Workout/ExerciseTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseTableViewCell.swift
+//  Office Workout
+//
+//  Created by Gemma Barlow on 10/4/14.
+//  Copyright (c) 2014 aspro. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+
+    // GB - Right mouse-click on these item in Interface Builder to see (visually) the connections I hooked up.
+
+    @IBOutlet var exerciseSwitch: UISwitch?     // <- Dot to the right of the screen area that represents it. 
+    @IBOutlet var exerciseLabel : UILabel?
+}
--- Office Workout/ViewController.swift
+++ Office Workout/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let exerciseCellIdentifier = "ExerciseTableViewCellIdentifier"
 
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +23,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    // MARK: - UITableViewDataSource Methods (represents 'what data to display')
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier(exerciseCellIdentifier, forIndexPath: indexPath) as ExerciseTableViewCell
+    }
+
+    
+    // MARK: - UITableViewDelegate Methods (represent 'what to do when a cell is tapped')
+    
+    // TODO
 
 
 }
